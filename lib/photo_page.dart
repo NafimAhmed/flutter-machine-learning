2402023895c6128341156fa4351dcbcd0d196320
recent <recentnafimahmed@gmail.com>
2024-10-27 09:47:59 +0600

    image labeling start

--- lib/photo_page.dart
+++ lib/photo_page.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
 
 class PhotoPage extends StatefulWidget{
@@ -16,12 +17,17 @@ class PhotoPage extends StatefulWidget{
 class _PhotoPageState extends State<PhotoPage> {
   late ImagePicker imagePicker;
   File? image;
+  late ImageLabeler imageLabeler;
+  String imageLabel='';
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     imagePicker=ImagePicker();
+
+     ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.6);
+     imageLabeler = ImageLabeler(options: options);
 
   }
 
@@ -36,24 +42,60 @@ class _PhotoPageState extends State<PhotoPage> {
     }
   }
 
+
+  Future<void> captureImage() async {
+    XFile? selectedImage=await imagePicker.pickImage(source: ImageSource.camera);
+    if(selectedImage!=null){
+      image=File(selectedImage.path);
+      performImageLabelling();
+      setState(() {
+        image;
+      });
+    }
+  }
+
+
+  performImageLabelling() async {
+    InputImage inputImage=InputImage.fromFile(image!);
+
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      imageLabel+='+++$text++++\n';
+      print('++++++++++++++++++++$text+++++++++++++++++++++++');
+    }
+
+    setState(() {
+      imageLabel;
+    });
+  }
+
   Widget
  build(BuildContext context){
     return Scaffold(
-      body: Column(
-        children: [
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
 
-          image!=null?
-          Image.file(image!):Icon(Icons.image_outlined,size: 150,),
-
-
-
-          IconButton(onPressed: (){
-            chooseImage();
-          }, icon: Icon(Icons.camera))
+            image!=null?
+            Image.file(image!):Icon(Icons.image_outlined,size: 150,),
 
 
 
-        ],
+            IconButton(onPressed: (){
+              // chooseImage();
+              captureImage();
+            }, icon: Icon(Icons.camera)),
+
+            Text(imageLabel)
+
+
+
+          ],
+        ),
       ),
     );
   }}
