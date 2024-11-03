894b311158b184c68b33c6c1a71b8139057fa9f3
recent <recentnafimahmed@gmail.com>
2024-11-03 12:34:12 +0600

    face detection smiling detection

--- lib/face_detection.dart
+++ lib/face_detection.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
@@ -73,6 +75,15 @@ class _FaceDetectionState extends State<FaceDetection> {
 
 
     print('************Len : ${faces.length}**********************');
+
+
+    for(Face f in faces){
+      if(f.smilingProbability!>=0.5){
+
+        result='Smiling';
+
+      }
+    }
 
     // for (Face face in faces) {
     //   final Rect boundingBox = face.boundingBox;
@@ -241,39 +252,39 @@ class FacePainter extends CustomPainter {
       canvas.drawRect(rectangle.boundingBox, p);
     }
 
-    // Paint p2 = Paint();
-    // p2.color = Colors.green;
-    // p2.style = PaintingStyle.stroke;
-    // p2.strokeWidth = 1;
-    //
-    // Paint p3 = Paint();
-    // p3.color = Colors.yellow;
-    // p3.style = PaintingStyle.stroke;
-    // p3.strokeWidth = 1;
-    //
-    // for (Face face in facesList) {
-    //   Map<FaceContourType, FaceContour?> con = face.contours;
-    //   List<Offset> offsetPoints = <Offset>[];
-    //   con.forEach((key, value) {
-    //     if(value != null) {
-    //       List<Point<int>>? points = value.points;
-    //       for (Point p in points) {
-    //         Offset offset = Offset(p.x.toDouble(), p.y.toDouble());
-    //         offsetPoints.add(offset);
-    //       }
-    //       canvas.drawPoints(PointMode.points, offsetPoints, p2);
-    //     }
-    //   });
-    //
-    //   // If landmark detection was enabled with FaceDetectorOptions (mouth, ears,
-    //   // eyes, cheeks, and nose available):
-    //   final FaceLandmark leftEar = face.landmarks[FaceLandmarkType.leftEar]!;
-    //   if (leftEar != null) {
-    //     final Point<int> leftEarPos = leftEar.position;
-    //     canvas.drawRect(Rect.fromLTWH(leftEarPos.x.toDouble()-5, leftEarPos.y.toDouble()-5,10,10),  p3);
-    //   }
-    //
-    // }
+    Paint p2 = Paint();
+    p2.color = Colors.green;
+    p2.style = PaintingStyle.stroke;
+    p2.strokeWidth = 1;
+
+    Paint p3 = Paint();
+    p3.color = Colors.yellow;
+    p3.style = PaintingStyle.stroke;
+    p3.strokeWidth = 1;
+
+    for (Face face in facesList) {
+      Map<FaceContourType, FaceContour?> con = face.contours;
+      List<Offset> offsetPoints = <Offset>[];
+      con.forEach((key, value) {
+        if(value != null) {
+          List<Point<int>>? points = value.points;
+          for (Point p in points) {
+            Offset offset = Offset(p.x.toDouble(), p.y.toDouble());
+            offsetPoints.add(offset);
+          }
+          canvas.drawPoints(PointMode.points, offsetPoints, p2);
+        }
+      });
+
+      // If landmark detection was enabled with FaceDetectorOptions (mouth, ears,
+      // eyes, cheeks, and nose available):
+      final FaceLandmark leftEar = face.landmarks[FaceLandmarkType.leftEar]!;
+      if (leftEar != null) {
+        final Point<int> leftEarPos = leftEar.position;
+        canvas.drawRect(Rect.fromLTWH(leftEarPos.x.toDouble()-5, leftEarPos.y.toDouble()-5,10,10),  p3);
+      }
+
+    }
 
   }
 
